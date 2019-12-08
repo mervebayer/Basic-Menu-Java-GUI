f056ed0c364addac713988394def3eaa5f54d44a
mervebayer <56397630+mervebayer@users.noreply.github.com>
2019-12-08 21:59:52 +0300

    Update Sale.java

--- Sale.java
+++ Sale.java
@@ -1,6 +1,6 @@
-	import java.awt.FlowLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
-	import java.awt.event.ActionListener;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.*; 
 
